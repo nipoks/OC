28f15a097945d3bb1425e900f3b0fe55aea07515
Nikita <n.skopin@g.nsu.ru>
2024-01-04 20:27:45 +0700

    update lab3.c

--- lab3.c
+++ lab3.c
@@ -7,6 +7,8 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <errno.h>
 
 typedef struct {
     char* src;
@@ -14,41 +16,56 @@ typedef struct {
 } Data;
 
 void data_destructor(Data* data) {
-	free(data->src);
-	free(data->dest);
-	free(data);
+    free(data->src);
+    free(data->dest);
+    free(data);
 }
 
 void* copy_file(void* arg) {
     Data* data = (Data*) arg;
     printf("\ntid[%d] start copy %s to %s\n", gettid(), data->src, data->dest);
 
-    int src_fd = open(data->src, O_RDONLY);
-    if (src_fd < 0) {
+    int src_fd;
+    while(true) {
+        src_fd = open(data->src, O_RDONLY);
+        if (src_fd == -1) {
+            if (errno == EMFILE) {
+                sleep(3);
+            }
         printf("Failed open src file\n");
         data_destructor(data);
         return NULL;
+        } else {
+            break;
+        }
     }
 
-    int dest_fd = open(data->dest, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (dest_fd < 0) {
-        printf("Failed open dest file\n");
-        close(src_fd);
+    int dest_fd;
+    while(true) {
+        dest_fd = open(data->dest, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+        if (dest_fd == -1) {
+            if (errno == EMFILE) {
+                sleep(3);
+            }
+        printf("Failed open dest_fd file\n");
         data_destructor(data);
+        close(src_fd);
         return NULL;
+        } else {
+            break;
+        }
     }
-    
-	char buffer[1024];
-	ssize_t bytes_read;
-	
+
+    char buffer[1024];
+    ssize_t bytes_read;
+
     while ((bytes_read = read(src_fd, buffer, 1024)) > 0) {
         if (bytes_read != write(dest_fd, buffer, bytes_read)) {
             printf("Failed write to dest file\n");
-            
+
             close(src_fd);
             close(dest_fd);
             data_destructor(data);
-           
             return NULL;
         }
     }
@@ -60,21 +77,31 @@ void* copy_file(void* arg) {
 }
 
 Data* create_new_data(char* src, char* dest) {
-	Data* new_data = (Data*) malloc(sizeof(Data));
+    Data* new_data = (Data*) malloc(sizeof(Data));
     new_data->src = strdup(src);
     new_data->dest = strdup(dest);
-	return new_data;
+    return new_data;
 }
 
 void* copy_dir(void* arg) {
     Data* data = (Data*) arg;
-	
-	DIR* dir = opendir(data->src);
-    if (!dir) {
-        printf("Failed open src dir: %s\n", data->src);
-        data_destructor(data);
-        return NULL;
+
+
+    DIR* dir;
+    while(true) {
+        dir = opendir(data->src);
+        if (!dir) {
+            if (errno == EMFILE) {
+                sleep(3);
+            }
+            printf("Failed open src file\n");
+            data_destructor(data);
+            return NULL;
+        } else {
+            break;
+        }
     }
+
 
     if (mkdir(data->dest, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == -1) {
         printf("Failed creat dest dir: %s\n", data->dest);
@@ -82,11 +109,11 @@ void* copy_dir(void* arg) {
         return NULL;
     }
 
-    pthread_t threadArr[1024]; //Ограниченное количество файлов в директории 
+    pthread_t threadArr[1024]; //Ограниченное количество файлов в директории
     int countThread = 0; // сдвиг в массиве потоков, для создания
 
     struct dirent* entry;
-    while ((entry = readdir(dir)) != NULL) {
+    while (((countThread < 1024) && (entry = readdir(dir)) != NULL)) {
         if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
             continue;
         }
@@ -105,9 +132,9 @@ void* copy_dir(void* arg) {
             Data* new_data = create_new_data(new_src, new_dest);
             pthread_create(threadArr + countThread, NULL, copy_dir, new_data);
             ++countThread;
-            
+
         } else if (S_ISREG(src_stat.st_mode)) {
-          	Data* new_data = create_new_data(new_src, new_dest);
+            Data* new_data = create_new_data(new_src, new_dest);
             pthread_create(threadArr + countThread, NULL, copy_file, new_data);
             ++countThread;
         }
@@ -117,7 +144,7 @@ void* copy_dir(void* arg) {
     for (int threadP = 0; threadP < countThread; ++threadP) {
         pthread_join(threadArr[threadP], NULL);
     }
-    
+
     data_destructor(data);
     return NULL;
 }
@@ -125,17 +152,16 @@ void* copy_dir(void* arg) {
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         printf("Bad arguments %s\n", argv[0]);
-        return 1;
+	return 1;
     }
-    
-	pthread_t thread;
+
+    pthread_t thread;
     Data* data = (Data*) malloc(sizeof(Data)); //очистим в функции поточной
-	data->src = strdup(argv[1]);			/// выделяется на куче и присваивается
+    data->src = strdup(argv[1]); /// выделяется на куче и присваивается
     data->dest = strdup(argv[2]);
 
     pthread_create(&thread, NULL, copy_dir, data);
     pthread_join(thread, NULL);
-    
+
     return 0;
 }
-
